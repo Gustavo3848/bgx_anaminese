bb8521d8dc2b28d57cc0ee920616326293dfeef1
Gustavo Bovollato Fonseca <62625677+Gustavo3848@users.noreply.github.com>
2023-02-08 12:03:02 -0300

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.set('view engine', 'ejs');
 app.use(expressLayouts);
 
 //Public Static
-app.use('/static',express.static('./public'));
+app.use('/static',express.static(path.join(__dirname, 'public')));
 app.use('/node_modules', express.static('node_modules'));
 app.use(bodyParser.urlencoded({ extended: true }))
 app.get('/', (req, res) => {
