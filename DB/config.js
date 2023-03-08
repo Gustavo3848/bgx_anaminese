d30179d01539185b54d7dd842802d6c3eb1ab242
Gustavo Bovollato <gustavo3848@edu.faccar.com.br>
2023-03-07 21:43:16 -0300

    07/03/2023

--- DB/config.js
+++ DB/config.js
@@ -3,7 +3,7 @@ const knex = require('knex')({
     connection: {
       host : '127.0.0.1',
       user : 'root',
-      password : '123456',
+      password : 'l52V2&83Iroi',
       database : 'bgx'
     }
   });
